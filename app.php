bb61629ca052f9dcec1beca8df9b7dc60cc6b211
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2021-07-23 07:50:23 -0700

    Update app.php

--- app.php
+++ app.php
@@ -1,14 +1,5 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
         $data = $_POST['email'];
         echo $data;
-    }
 ?>
-<!DOCTYPE html>
-<html>
-<body>
-<form action="http://localhost:63342/PHPPost/app.php?_ijt=pv0pc9jt4og5of1k7bavgsk5eb" method="post">
-    <input type="email" name="email">
-</form>
-</body>
-</html>
